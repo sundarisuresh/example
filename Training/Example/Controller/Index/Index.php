37dcb7f764aa04683499043af82339de47b89c43
sundarisuresh <sundarisuresh44@gmail.com>
2022-03-30 17:22:54 +0530

    new page with template

--- Training/Example/Controller/Index/Index.php
+++ Training/Example/Controller/Index/Index.php
@@ -4,22 +4,35 @@ namespace Training\Example\Controller\Index;
 use Magento\Framework\App\ActionInterface;
 use Magento\Framework\Controller\Result\RawFactory;
 
-class Index implements ActionInterface 
+class Index extends \Magento\Framework\App\Action\Action 
 {
-	protected $resultfactory;
+	// protected $resultfactory;
 
-	// this is constructor
+	// // this is constructor
+	// public function __construct(
+	// 	RawFactory $resultfactory
+	// 	)
+	// {
+    //    $this->resultfactory = $resultfactory;
+    // }
+
+
+
+	protected $_pageFactory;
+
 	public function __construct(
-		RawFactory $resultfactory
-		)
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
 	{
-       $this->resultfactory = $resultfactory;
-    }
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
 
 	// this is controller
 	public function execute()
 	{
 		
-		return $this->resultfactory->create()->setContents("fruits");
+		return $this->_pageFactory->create();
 	}
 }
