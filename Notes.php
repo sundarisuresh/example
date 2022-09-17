07043c06fc3f993ade836eb06ea6507908301f3f
root <root@dfb596280784>
2022-09-17 03:47:04 +0000

    Added notes

--- Notes.php
+++ Notes.php
@@ -17,3 +17,82 @@ return false;
 //        $logger = new \Zend_Log();
 //        $logger->addWriter($writer);
 //        $logger->info('text 123');
+
+
+<?php
+/**
+ * Copyright © free All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Around\App\Controller\Verify;
+
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Twilio\Rest\Client;
+
+class Index extends \Magento\Framework\App\Action\Action
+{    protected $request;
+
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+    private \Magento\Customer\Model\Session $_customerSession;
+
+    /**
+     * Constructor
+     *
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(PageFactory $resultPageFactory,
+                                \Magento\Framework\App\Action\Context       $context,
+                                \Magento\Customer\Model\Session $customerSession,
+                                \Magento\Framework\App\Request\Http         $request
+    )
+    {        $this->request = $request;
+        $this->_customerSession = $customerSession;
+
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return ResultInterface
+     */
+    public function execute()
+    {
+        $phone = $this->request->getParam("phone");
+        $otp  = $this->randNum();
+        $this->_customerSession->setOtp($otp);
+        echo $otp;
+//        exit;
+        $sid    = "AC478d735f436986f7d2b6487aa9a977da";
+        $token  = "7bbe05dc74d98d63ce57bda7ff3e964d";
+        $twilio = new Client($sid, $token);
+
+        $message = $twilio->messages
+            ->create("+91".$phone, // to
+                array(
+                    "from" => "+18158699964",
+                    "messagingServiceSid" => "MG04f723e194d7568e19027238e7e18ee1",
+                    "body" => "Your OTP is ".$otp
+                )
+            );
+
+        print($message->sid);
+        return $this->resultPageFactory->create();
+    }
+
+    private function randNum()
+    {
+        $digits = 4;
+        return rand(pow(10, $digits-1), pow(10, $digits)-1);
+    }
+}
+
